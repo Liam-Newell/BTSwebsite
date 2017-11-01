7aa7f3811b06acd29b576045a573655f6ea16031
Liam Newell <lpnewell@myseneca.ca>
2017-11-01 19:01:57 -0400

    login fix

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
     req.session.errors = null;
 });
 router.get('/login', function (req, res, next) {
-    res.render('register', {});
+    res.render('login', {});
 });
 router.get('/register', function (req, res, next) {
     res.render('register', {});
