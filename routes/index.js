9a87767b4453d303efe637f76fa8d50dc6fdab3e
Liam Newell <lpnewell@myseneca.ca>
2017-11-01 00:35:47 -0400

    login

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var path    = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BTS Church' });
+    res.render('index', { title: 'Bts Church' , cuck:'liam' });
+});
+router.get('/index', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+router.post('/test', function(req, res, next) {
+    var lastName = req.body.lastName;
+    var firstName = req.body.lastName;
+    res.render('test', { firstName: firstName, lastName:  lastName});
+});
 
 module.exports = router;
