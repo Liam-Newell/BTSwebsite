71d78eaa7c10c7259da9a9637dfb87f9b0c00931
Liam Newell <lpnewell@myseneca.ca>
2017-11-01 10:43:02 -0400

    login

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,27 @@ var path    = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Bts Church' , cuck:'liam' });
+    res.render('index', { title: 'Bts Church' , cuck:'liam'
+    , success: false, errors: req.session.errors});
+    req.session.errors = null;
 });
 router.get('/index', function(req, res, next) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-router.post('/test', function(req, res, next) {
+router.post('/submit', function(req, res, next) {
+
     var lastName = req.body.lastName;
-    var firstName = req.body.lastName;
-    res.render('test', { firstName: firstName, lastName:  lastName});
+    var firstName = req.body.firstName;
+    if(lastName.length > 0 || firstName.length > 1) {
+        res.render('test', {a: firstName, b: lastName});
+    }
+    else {
+        res.render('index', {
+            title: 'Incorrect Values', cuck: 'Ya messed up'
+            , success: false, errors: req.session.errors
+        });
+    }
 });
 
 module.exports = router;
