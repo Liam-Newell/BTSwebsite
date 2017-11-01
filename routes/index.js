461f7afc965fbb392c4192c93f8698d522353547
Liam Newell <lpnewell@myseneca.ca>
2017-11-01 17:43:07 -0400

    added register method

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var path    = require("path");
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,18 +16,51 @@ router.get('/index', function(req, res, next) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-router.post('/submit', function(req, res, next) {
+router.post('/register', function (req, res, next){
+
+    var item = {
+        firstName: req.body.firstname,
+        lastName: req.body.lastname
+    };
+
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('data').insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item inserted');
+            db.close();
+        });
+    });
+
+    res.redirect('/');
+});
+//note '/submit' is identicial to in the index.hbs file
+router.post('/login', function(req, res, next) {
     //form validation etc
     req.check('lastName', 'Invalid last name').isLength({min:2});
     req.check('firstName','Invalid first name').isLength({min:4});
+
     var lastName = req.body.lastName;
 
     var firstName = req.body.firstName;
-
     var errors = req.validationErrors();
-    if(!errors)
+    var results = [];
 
-        res.render('test', {a: firstName, b: lastName});
+    var cursor = db.collection('data').find();
+    cursor.forEach(function(doc, error)
+    {
+        assert.equal(null, error);
+        results.push(doc);
+    }
+    , function() {
+        db.close();
+    });
+    db.close();
+    });
+
+    if(!errors || results.length > 0)
+
+        res.render('test', {a: firstName, b: lastName, resultlist: results});
 
     else {
         req.session.errors = errors;
