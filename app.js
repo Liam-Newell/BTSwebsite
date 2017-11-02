61257828286434624b9a0b221be70e5129c13be1
Mina Nashed <mmnashed@myseneca.ca>
2017-11-02 01:30:21 -0400

    Test Commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup!
+// view engine setup!!
 app.engine('hbs',hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
