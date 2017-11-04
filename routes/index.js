69b4f49c48c82ac4023e02c16e1e12670b1a2f04
Liam Newell <lpnewell@myseneca.ca>
2017-11-04 01:07:43 -0400

    new layout

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('localhost:27017/test');
+var options = {
+    useMongoClient: true,
+    autoIndex: true, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+};
+mongoose.connect('mongodb://localhost:27017/test',options );
 var Schema = mongoose.Schema;
 
 var userLoginSchema = new Schema({
     firstname:      {type: String, required: true},
-    lastname:       {type: String, required: true}
+    lastname:       {type: String, required: true},
+    password:{type: String, required: true},
+    username: {type: String, required: true}
 }, {collection: 'data'});
 
 var UserLogin = mongoose.model('UserLogin', userLoginSchema);
@@ -20,22 +31,20 @@ var userDataSchema = new Schema({
     email:          {type: String, required: true},
     phonenumber:    {type: String, required: true},
     phonenumber2:   {type: String, required: false},
-    birthday:       {type: Date, required: true},
-    rolecode:       {type: String, required: true},
-    children:       ["child"]
+    birthday:       {type: Date, required: true}
 }, {collection: 'user'});
 
 var UserData = mongoose.model('UserData', userDataSchema);
 
-var childDataSchema = new Schema({
+/*var childDataSchema = new Schema({
     firstname:      {type: String, required: true},
     lastname:       {type: String, required: true},
     age:            {type: Number, min: 18, max: 99, required: true},
     birthday:       {type: Date, required: true},
     grade:          {type: Number, min: 1, max: 8, required: true}
-}, {collection: 'child'});
+}, {collection: 'child'});*/
 
-var ChildData = mongoose.model('ChildData', childDataSchema);
+//var ChildData = mongoose.model('ChildData', childDataSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -83,15 +92,17 @@ router.post('/register', function (req, res, next){
     // };
     var item = {
         firstname: req.body.firstname,
-        lastname: req.body.lastname
+        lastname: req.body.lastname,
+        password: req.body.password,
+        username: req.body.username
     };
-    req.check('lastName', 'Invalid last name').isLength({min:2});
-    req.check('firstName','Invalid first name').isLength({min:4});
+    req.check('lastname', 'Invalid last name').isLength({min:2});
+    req.check('firstname','Invalid first name').isLength({min:4});
     var errors = req.validationErrors();
     if(!errors){
-        var data = new UserData(item);
-        if(data.on())
-            console.log('Entry Inserted');
+        var data = new UserLogin(item);
+       // if(data.on())
+      //      console.log('Entry Inserted');
         data.save();
         res.render(('homepage2'),{a : item.firstname, b:item.lastname, resultlist: 'cuck'});
     }
@@ -109,7 +120,9 @@ router.post('/login', function(req, res, next) {
     //form validation etc
     var item = {
         firstname: req.body.firstname,
-        lastname: req.body.lastname
+        lastname: req.body.lastname,
+        password: req.body.password,
+        username: req.body.username
     };
     UserLogin.find({firstname : item.firstname, lastname : item.lastname}).then(function(doc){
         if(doc < 1){
