c4340e0d4ecc8589009434f6f71ae2066f736bf3
Liam Newell <lpnewell@myseneca.ca>
2017-11-07 21:21:48 -0500

    Event creation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+var Schema = mongoose.Schema;
+
+var eventdataschema = new Schema({
+    year: {type: Number, min: 2015, max: 2030, required: true},
+    day: {type: Number, min: 1, max: 31, required: true},
+    info: {type: String, required: true}
+}, {collection: 'events'});
+
+var EventData = mongoose.model('EventData', eventdataschema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    EventData.find().then(function (doc) {
+        res.render('Users/eventDB', {eventlist: doc});
+    })
+
 });
 
 module.exports = router;
