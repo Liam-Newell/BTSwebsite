9403c5d2dbb650d769905d5d42440079b6c5b010
Liam Newell <liam-newell@hotmail.com>
2018-04-03 13:26:07 -0400

    part way done the mass email

--- models/controller.js
+++ models/controller.js
@@ -43,3 +43,38 @@ module.exports.registerChildForEvent = function (req,callback) {
     // item.events.push(req.body.id);
     callback(null,log);
 };
+/**
+@param {eventid._id} eventid
+ **/
+module.exports.massEventEmail = function(eventid, callback){
+    var childList = [];
+    var parentList = [];
+    var promise1 = new Promise(function(resolve, reject){
+        EventData.findOne(eventid, function (err, currEvent) {
+            if(err) callback('failed in the event query for mass email')
+            childList = currEvent.registered;
+        });
+    }).then(function () {
+        var childQuery = [];
+        for (var i = 0; i < childList.length; i++) {
+            var o = childList[i];
+            childQuery.push(new mongoose.Types.ObjectId(o));
+        }
+        var promise = new Promise(function (resolve, reject){
+            Child.find({
+            '_id': {$in: childQuery}
+        }, function (err, docs) {
+            if(err) callback('failed inside of child query for mass email',null);
+            console.log(docs);
+            for (i in docs) {
+                parentList.push(docs[i]._doc.parent);
+            }
+            });
+        }).then(function(){
+            var promise2 = new Promise(function(resolve, reject){
+
+            })
+        });
+    });
+
+};
